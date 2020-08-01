648092f934029fdb7e2becf6ed29d7b288d7ebc1
Val <52679131+vbluteau@users.noreply.github.com>
2020-08-01 17:10:39 -0400

    Update paid_activities.py

--- paid_activities.py
+++ paid_activities.py
@@ -2,19 +2,19 @@ def eat_out():
   print("Let's go to this restaurant")
   
 def purchase_furniture():
-  print("Let's go to Ikea.")
+  print("Let's go to Ikea")
 
 def go_to_mall():
-  print("Let's go to the mall.")
+  print("Let's go to the mall")
 
 def go_to_movie():
-  print("Let's go to a movie.")
+  print("Let's go to a movie")
 
 def go_to_museum():
-  print("Let's go to a museum.")
+  print("Let's go to a museum")
 
 def farmers_market():
-  print("Let's go to a Farmer's Market.")
+  print("Let's go to a Farmer's Market")
 
 def concert():
-  print("Let's go to a concert.")
+  print("Let's go to a concert")
